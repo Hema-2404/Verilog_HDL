6c8f72b339cf97ebf312e2d4ee1f5061c66b2f79
Hema-2404 <hemakopoo@gmail.com>
2026-09-09 12:55:43 +0530

    Add testbench for fsm_nonover_mealy1001

---
+++ Sequential circuits /FSM/fsm_nonover_mealy1001_tb.v
@@ -0,0 +1,41 @@
+module fsm_nonov_mealy1001_tb;
+reg clk,rst,din;
+wire dout;
+fsm_nonov_mealy1001 DUT (clk,rst,din,dout);
+ task apply_bit;
+    input b;
+    begin
+      din = b;
+      @(posedge clk);
+    end
+  endtask
+always #5 clk=~clk;
+initial begin 
+clk=0;
+rst=1;
+din=0;
+$dumpfile("fsm_nonov_mealy1001.vcd");
+$dumpvars(0,fsm_nonov_mealy1001_tb);
+$monitor("clk=%b rst=%b din=%b dout=%b",clk,rst,din,dout);
+@(posedge clk);
+rst=0; 
+apply_bit(1);
+apply_bit(0);
+apply_bit(0);
+apply_bit(1);
+
+apply_bit(1);
+apply_bit(0);
+apply_bit(0);
+apply_bit(1);
+
+apply_bit(1);
+apply_bit(1);
+apply_bit(0);
+apply_bit(0);
+apply_bit(1);
+
+#20;
+$finish;
+end
+endmodule
