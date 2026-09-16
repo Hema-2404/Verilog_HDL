e84439946eca8775e35bc2af314337e052855abf
Hema-2404 <hemakopoo@gmail.com>
2026-09-16 14:52:45 +0530

    Add testbench for sequence_detector_1111 module

---
+++ Sequential circuits /FSM/sequence_detector_1111_tb.v
@@ -0,0 +1,54 @@
+module sequence_detector_1111_tb;
+
+reg clk;
+reg rst;
+reg din;
+wire dout;
+
+sequence_detector_1111 dut (
+    .clk(clk),
+    .rst(rst),
+    .din(din),
+    .dout(dout)
+);
+
+
+
+always #5 clk = ~clk;
+
+
+initial begin
+
+    $dumpfile("sequence_1111.vcd");
+    $dumpvars(0, sequence_detector_1111_tb);
+
+    $monitor("Time=%0t | din=%b | state=%b | dout=%b",
+             $time, din, dut.current_state, dout);
+
+    clk = 0;
+    rst = 1;
+    din = 0;
+
+    // Reset
+    #10;
+    rst = 0;
+
+    // 1111
+    #10 din = 1;
+    #10 din = 1;
+    #10 din = 1;
+    #10 din = 1;
+
+    // Another sequence
+    #10 din = 1;
+    #10 din = 1;
+    #10 din = 1;
+    #10 din = 1;
+
+    #20;
+
+    $finish;
+
+end
+
+endmodule
