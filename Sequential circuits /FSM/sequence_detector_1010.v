de1b79e26218d69e23c44c052d66f314f05b8174
Hema-2404 <hemakopoo@gmail.com>
2026-09-16 14:42:02 +0530

    Add sequence detector for 1010 pattern

---
+++ Sequential circuits /FSM/sequence_detector_1010.v
@@ -0,0 +1,56 @@
+module sequence_detector_1010(
+input clk,
+input rst,
+input din,
+output reg dout
+);
+parameter S0=2'b00;
+parameter S1=2'b01;
+parameter S2=2'b10;
+parameter S3=2'b11;
+reg [1:0] current_state;
+reg [1:0] next_state;
+always @(posedge clk or posedge rst) begin
+if (rst) 
+current_state<=S0;
+else
+current_state<=next_state;
+end
+always @(*) begin 
+next_state =S0;
+dout=1'b0;
+case(current_state)
+S0:begin 
+if(din == 1'b1)
+next_state=S1;
+else
+next_state=S0;
+end
+S1:begin
+if(din == 1'b0)
+next_state=S2;
+else
+next_state=S1;
+end
+S2:begin
+if(din == 1'b1)
+next_state=S3;
+else
+next_state=S0;
+end
+S3:begin
+if(din == 1'b0)begin
+next_state=S0;
+dout=1'b1;
+end
+else begin
+next_state=S1;
+end
+end
+default:begin
+next_state =S0;
+dout = 1'b0;
+end
+endcase
+end 
+endmodule
